b67a04b2daaa9d381ed715782242811592a3a32b
George Shakhnazaryan <gshakhnazaryan@backstopsolutions.com>
2012-09-14 16:02:23 -0500

    Add a few failing tests for the maze object.

--- maze/maze-test.js
+++ maze/maze-test.js
@@ -3,11 +3,30 @@ var vows = require('vows'),
 
 var maze = require('./maze');
 
+var Grid = maze.Grid;
 var CellTypes = maze.CellTypes;
+
+vows.describe('Maze').addBatch({
+    'when it is a simple 3x3':{
+        topic: new Grid([2, 0], [0, 0],
+            [[CellTypes.N, CellTypes.NE, CellTypes.W],
+            [CellTypes.NS, CellTypes.ESW, CellTypes.NW],
+            [CellTypes.ES, CellTypes.W, CellTypes.S]]),
+        'it should have a victory point at 0, 0': function(maze) {
+            assert.isTrue(maze.victory(0, 0));
+        },
+        'it should not have a victory point at 2, 0': function(maze) {
+            assert.isFalse(maze.victory(2, 0));
+        },
+        'it should have a starting location of 2, 0': function(maze) {
+            assert.isEqual(maze.getStart(), [2, 0]);
+        }
+    }
+}).export(module);
 
 vows.describe('CellTypes').addBatch({
     'when have walls to the north, east, and west':{
-        topic: (new CellTypes()).NEW,
+        topic: CellTypes.NEW,
         'it should not have an opening to the south':function (location) {
             assert.isFalse(location.canGoSouth());
         },
--- maze/maze.js
+++ maze/maze.js
@@ -1,44 +1,43 @@
-exports.Maze = function () {
-    this.uuid = require('node-uuid');
+exports.Grid = function (start, end, cells) {
 
 };
 
-exports.CellTypes = function() {
-    var CellType = function(canGoNorth, canGoEast, canGoSouth, canGoWest) {
-        this.northOpen = canGoNorth;
-        this.eastOpen = canGoEast;
-        this.southOpen = canGoSouth;
-        this.westOpen = canGoWest;
-    };
-
-    CellType.prototype.canGoNorth = function() {
-        return this.northOpen;
-    };
-
-    CellType.prototype.canGoEast = function() {
-        return this.eastOpen;
-    };
-
-    CellType.prototype.canGoSouth = function() {
-        return this.southOpen;
-    };
-
-    CellType.prototype.canGoWest = function() {
-        return this.westOpen;
-    };
-
-    this.N = {};
-    this.E = {};
-    this.S = {};
-    this.W = {};
-    this.NE = {};
-    this.NW = {};
-    this.NS = {};
-    this.NEW = new CellType(true, true, false, true);
-    this.NES = {};
-    this.NWE = {};
-    this.ES = {};
-    this.EW = {};
-    this.ESW = {};
-    this.SW = {};
+var CellType = function(canGoNorth, canGoEast, canGoSouth, canGoWest) {
+    this.northOpen = canGoNorth;
+    this.eastOpen = canGoEast;
+    this.southOpen = canGoSouth;
+    this.westOpen = canGoWest;
 };
+
+CellType.prototype.canGoNorth = function() {
+    return this.northOpen;
+};
+
+CellType.prototype.canGoEast = function() {
+    return this.eastOpen;
+};
+
+CellType.prototype.canGoSouth = function() {
+    return this.southOpen;
+};
+
+CellType.prototype.canGoWest = function() {
+    return this.westOpen;
+};
+
+CellTypes = {};
+CellTypes.N = {};
+CellTypes.E = {};
+CellTypes.S = {};
+CellTypes.W = {};
+CellTypes.NE = {};
+CellTypes.NW = {};
+CellTypes.NS = {};
+CellTypes.NEW = new CellType(true, true, false, true);
+CellTypes.NES = {};
+CellTypes.NWE = {};
+CellTypes.ES = {};
+CellTypes.EW = {};
+CellTypes.ESW = {};
+CellTypes.SW = {};
+exports.CellTypes = CellTypes;
