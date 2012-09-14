d105a6825d6d80f55f9c6958ae5bce2872afc4cc
George Shakhnazaryan <gshakhnazaryan@backstopsolutions.com>
2012-09-14 17:01:00 -0500

    Add more failing tests.

--- maze/maze-test.js
+++ maze/maze-test.js
@@ -5,13 +5,70 @@ var maze = require('./maze');
 
 var Grid = maze.Grid;
 var CellTypes = maze.CellTypes;
+var simpleLab = maze.SampleLabyrinths.simple;
+var Labyrinth = maze.Labyrinth;
+var PlayerDirections = maze.PlayerDirections;
+
+vows.describe('Labyrinth').addBatch({
+   'when the game starts with a simple labyrinth':{
+       topic: new Labyrinth(simpleLab),
+       'it should have the player at the start location': function(lab) {
+           assert.deepEqual(lab.getPlayerLocation(), lab.getGrid().getStart());
+       },
+       'it should have the player facing north': function(lab) {
+           assert.deepEqual(lab.getPlayerDirection(), PlayerDirections.NORTH);
+       }
+   }
+}).export(module);
+
+vows.describe('Player').addBatch({
+    'when the player is facing is north': {
+        topic: new Player([0,0], PlayerDirections.NORTH),
+        'it should face east when it turns to the right': function(player) {
+            player.turnRight();
+            assert.deepEqual(player.getDirection(), PlayerDirections.EAST);
+        },
+        'it should face west when it turns to the left': function(player) {
+            player.turnLeft();
+            assert.deepEqual(player.getDirection(), PlayerDirections.WEST);
+        },
+        'it should face south if the player turns right twice': function(player) {
+            player.turnRight();
+            player.turnRight();
+            assert.deepEqual(player.getDirection(), PlayerDirections.SOUTH);
+        },
+        'it should face south if the player turns left twice': function(player) {
+            player.turnLeft();
+            player.turnLeft();
+            assert.deepEqual(player.getDirection(), PlayerDirections.SOUTH);
+        }
+    },
+    'when the player is facing is south': {
+        topic: new Player([0,0], PlayerDirections.SOUTH),
+        'it should face west when it turns to the right': function(player) {
+            player.turnRight();
+            assert.deepEqual(player.getDirection(), PlayerDirections.WEST);
+        },
+        'it should face east when it turns to the left': function(player) {
+            player.turnLeft();
+            assert.deepEqual(player.getDirection(), PlayerDirections.EAST);
+        },
+        'it should face north if the player turns right twice': function(player) {
+            player.turnRight();
+            player.turnRight();
+            assert.deepEqual(player.getDirection(), PlayerDirections.NORTH);
+        },
+        'it should face north if the player turns left twice': function(player) {
+            player.turnLeft();
+            player.turnLeft();
+            assert.deepEqual(player.getDirection(), PlayerDirections.NORTH);
+        }
+    }
+});
 
 vows.describe('Maze').addBatch({
     'when it is a simple 3x3':{
-        topic: new Grid([2, 0], [0, 0],
-            [[CellTypes.N, CellTypes.NE, CellTypes.W],
-            [CellTypes.NS, CellTypes.ESW, CellTypes.NW],
-            [CellTypes.ES, CellTypes.W, CellTypes.S]]),
+        topic: new Grid(simpleLab.start, simpleLab.end, simpleLab.cells),
         'it should have a victory point at 0, 0': function(maze) {
             assert.isTrue(maze.victory(0, 0));
         },
--- maze/maze.js
+++ maze/maze.js
@@ -1,12 +1,5 @@
-exports.Grid = function (start, end, cells) {
-    this.victory = function(x, y) {
-        return end[0] === x && end[1] === y;
-    };
-
-    this.getStart = function() {
-        return start;
-    };
-};
+var PlayerDirections = {};
+PlayerDirections.NORTH = {};
 
 var CellType = function(canGoNorth, canGoEast, canGoSouth, canGoWest) {
     this.northOpen = canGoNorth;
@@ -46,4 +39,40 @@ CellTypes.ES = {};
 CellTypes.EW = {};
 CellTypes.ESW = {};
 CellTypes.SW = {};
+
+var Grid = function (start, end, cells) {
+    this.victory = function(x, y) {
+        return end[0] === x && end[1] === y;
+    };
+
+    this.getStart = function() {
+        return start;
+    };
+};
+
+SampleLabyrinths = {};
+SampleLabyrinths.simple = {
+    cells:  [[CellTypes.N, CellTypes.NE, CellTypes.W],
+        [CellTypes.NS, CellTypes.ESW, CellTypes.NW],
+        [CellTypes.ES, CellTypes.W, CellTypes.S]],
+    start: [2,0],
+    end: [0,0]
+};
+
+Labyrinth = function(definition) {
+    var grid = new Grid(definition.start, definition.end, definition.cells)
+    this.getGrid = function() {
+        return grid;
+    }
+};
+
+Player = function(startLocation, startDirection) {
+
+};
+
+exports.PlayerDirections = PlayerDirections;
+exports.Player = Player;
 exports.CellTypes = CellTypes;
+exports.Grid = Grid;
+exports.SampleLabyrinths = SampleLabyrinths;
+exports.Labyrinth = Labyrinth;
